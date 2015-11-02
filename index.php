0e460eaebcae5568e6386dd2082d92dba571f1e6
rotsee <leo_wallentin@hotmail.com>
2015-11-02 09:56:07 +0100

    make list widget a class, demos

--- index.php
+++ index.php
@@ -1,8 +1,45 @@
-<html>
+<?php
+
+include "list.php";
+
+?><html>
 <head>
 <title>Test page</title>
 </head>
-<body><?php
+<body>
+	<header>
+		<h1>List widget demo</h1>
+	</header><?php
 
-	include __DIR__ . "/list.php";
+	/* Procedure style */
+	?><h2>Procedure style</h2><?php
+	TopList\printWidget();
+
+	/* Procedure style, with configuration */
+	?><h2>Procedure style, with config</h2><?php
+	$listFilter = array('gender' => 'female', 'region' => 'asia');
+	TopList\printWidget( $listFilter );
+
+	/* User url parameters */
+	?><h2>User url parameters</h2><?php
+	TopList\printWidget( $_GET );
+
+	/* Object oriented style, 1 */
+	?><h2>Object oriented style, 1</h2><?php
+	$widget = new TopList\Widget();
+	$widget->gender = 'female';
+	$widget->region = 'asia';
+	$widget->printHTML();
+
+	/* Object oriented style, 2 */
+	?><h2>Object oriented style, 2</h2><?php
+	$widget = new TopList\Widget( $listFilter );
+	$widget->printHTML();
+
+	/* Object oriented style, 3 */
+	?><h2>Object oriented style, 3</h2><?php
+	$widget = new TopList\Widget( );
+	$html = $widget->getHTML();
+	// do something with html
+	echo( $html );
 
--- list.php
+++ list.php
@@ -1,9 +1,32 @@
 <?php
+/* Entry point for PHP scripts. See index.php for usage examples. */
+
 namespace Toplist;
 require __DIR__ . '/settings.php';
 require $baseDir . 'lib/list.php';
 require $baseDir . 'lib/html.php';
 
-$list = new TList($_GET);
-$widget = new TListWidget($list, DEBUG);
-$widget->printHTML();
+class Widget {
+
+    var $list;
+    var $widget;
+
+    function __construct( $parameters = array() ){
+        $this->list = new TList( $parameters );
+        $this->widget = new TListWidget($this->list, DEBUG);
+    }
+
+    function printHTML(){
+        $this->widget->printHTML();
+    }
+
+    function getHTML(){
+        return $this->widget->getHTML();
+    }
+
+}
+
+function printWidget( $parameters = array() ){
+    $obj = new Widget( $parameters );
+    $obj->printHTML();
+}
