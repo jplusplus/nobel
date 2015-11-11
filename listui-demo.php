7d9e046236c7b299ec8ab09edfa95e2eecd002bb
rotsee <leo_wallentin@hotmail.com>
2015-11-11 09:39:32 +0100

    onlu allow in development

--- listui-demo.php
+++ listui-demo.php
@@ -1,4 +1,10 @@
 <?php
+/* Disable this script in development environments */
+define('TopList', TRUE);
+include "settings.php";
+if ($debugLevel === PRODUCTION){
+    die('Not permitted');
+}
 include "list.php";
 
 ?><html><head><title>List UI demo</title>
