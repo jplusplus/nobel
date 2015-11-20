ecc9be5145343e8ce1e3f64e96bd43ba33929714
rotsee <leo_wallentin@hotmail.com>
2015-11-20 17:04:16 +0100

    lookup dbpedia id

--- gallery-api.php
+++ gallery-api.php
@@ -1,7 +1,28 @@
 <?php
+namespace Toplist;
 define('TopList', TRUE);
+
 require __DIR__ . '/settings.php';
+require $baseDir . 'vendor/bordercloud/sparql/Endpoint.php'; //This lib is not autoloaded
 
 header('Content-Type: application/json; charset=utf-8');
 
-echo json_encode(array("hello" => "world"));
+$laureate = 579;
+
+$sparqlEndpoint = new \Endpoint('http://data.nobelprize.org/sparql');
+
+$query = "PREFIX owl: <http://www.w3.org/2002/07/owl#>
+SELECT ?laur ?sameAs {
+    ?laur owl:sameAs ?sameAs
+    FILTER (?laur IN (<http://data.nobelprize.org/resource/laureate/$laureate>))
+}";
+$result = $sparqlEndpoint->query($query)["result"]["rows"];
+$dbPediaUri = null;
+$dbPediaLinks = array_filter( $result, function( $var ){
+    $host = parse_url( $var["sameAs"], PHP_URL_HOST );
+    return ('dbpedia.org' === $host);
+});
+//Use only the first link, i multiple
+$dbPediaLink = array_pop($dbPediaLinks)["sameAs"];
+
+echo json_encode(array($laureate => $dbPediaLink));
