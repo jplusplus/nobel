cd5fc3ec72df7aaa6e90d5db35f307c79c94a83d
rotsee <leo_wallentin@hotmail.com>
2015-11-26 17:20:36 +0100

    use multiple Wikipedias

--- gallery-api.php
+++ gallery-api.php
@@ -5,8 +5,8 @@ define('TopList', TRUE);
 require __DIR__ . '/settings.php';
 
 require $baseDir . 'vendor/autoload.php';
-//require $baseDir . 'vendor/bordercloud/sparql/Endpoint.php'; //This lib is not autoloaded
 require $baseDir . 'lib/dbpedia.php';
+require $baseDir . 'lib/wikidata.php';
 
 header('Content-Type: application/json; charset=utf-8');
 
@@ -26,7 +26,7 @@ $parameters = $gump->run($parameters);
 $laureate = $parameters['id'];
 $width = @$parameters['width'] ?: null;
 $height = @$parameters['height'] ?: null;
-if (!($height || $weight)){
+if (!($height || $width)){
     $height = '300';
 }
 
@@ -57,14 +57,21 @@ if ( !array_key_exists( $dbPediaLink, $response ) ){
 $enWikipediaName = $response[$dbPediaLink];
 
 /* Get language links */
-https://www.wikidata.org/w/api.php?action=wbgetentities&sites=enwiki&titles=Ore%20Mountains&languages=cs|de|es|fr|it|pl|pt|ru&props=sitelinks%2Furls
+$wikiDataQuery = new WikiDataQuery();
+$iwLinks = $wikiDataQuery->getSitelinks($enWikipediaName);
+$allWikipediaNames = array();
+global $gImageSourceWPEditions;
+foreach ($gImageSourceWPEditions as $wpEdition) {
+    if ( array_key_exists( $wpEdition . 'wiki', $iwLinks )){
+        $allWikipediaNames[$wpEdition] = $iwLinks[$wpEdition . 'wiki'];
+    }
+}
 
-/* Query wp for images */
+/* Query Wikipedias for images */
 $params = array(
     'action'    => 'query',
     'prop'      => 'imageinfo',
     'generator' => 'images',
-    'titles'    => $enWikipediaName,
     'iiprop'    => 'extmetadata|mediatype|size|url',
     'iiextmetadatalanguage' => 'en',
     'format'    => 'json'
@@ -74,47 +81,61 @@ if ($height) {
 } elseif ($width) {
     $params['iiurlwidth'] = $width;
 }
-$paramString = http_build_query( $params );
-$wikipediaEdition = 'en';
-$endpoint = "https://$wikipediaEdition.wikipedia.org/w/api.php?$paramString";
 
-$md5 = md5($endpoint);
-$images = null;//__c()->get($md5);
-if ($images === null){
-    $json = file_get_contents($endpoint);
-    $response = json_decode($json, true);
-    if (array_key_exists('query', $response)){
-    	$pages = $response["query"]["pages"];
-    	foreach ( $pages as $page ){
-    		$imgInfo = array_pop($page["imageinfo"]);
-    		if ( $imgInfo["mediatype"] === 'BITMAP' &&
-    			 $imgInfo["width"] > 200 &&
-                 $imgInfo["height"] > 280 ){
+$output = array();
+$allImageNames = array(); // to filter out duplicates
+foreach ($allWikipediaNames as $wikipediaEdition => $pageName){
+    $params['titles'] = $pageName;
+    $paramString = http_build_query( $params );
+    $endpoint = "https://$wikipediaEdition.wikipedia.org/w/api.php?$paramString";
 
-                $metaData = $imgInfo["extmetadata"];
-
-                $attributionRequired = ('true' === @$metaData["AttributionRequired"]["value"]);
-                $cred = '';
-                if ($attributionRequired){
-                    $cred .= @$metaData["LicenseShortName"]["value"] ?: @$metaData["LicenseShortName"]["value"];
-                    $cred .= ', ';
-                    $cred .= implode(' ', array( strip_tags(@$metaData["Credit"]["value"]), strip_tags(@$metaData["Artist"]["value"]) ));
-                }
-	    		$images[] = array (
-	    			"caption"   => strip_tags(@$metaData['ImageDescription']['value'] ?: ''),
-                    "credit"    => $cred,
-                    "url"       => $imgInfo['thumburl'],
-                    "sourceurl" => $imgInfo['descriptionurl'],
-
-	    		);
-    		}
-
-    	}
-    } else {
+    $md5 = md5($endpoint);
+    $images = __c()->get($md5);
+    if ($images === null){
         $images = array();
+        $json = file_get_contents($endpoint);
+        $response = json_decode($json, true);
+        if (array_key_exists('query', $response)){
+            $pages = $response["query"]["pages"];
+            foreach ( $pages as $page ){
+                $title = explode(':', $page["title"])[1]; // Add only part after ':'
+                if ( !in_array( $title, $allImageNames ) ){
+                    $allImageNames[] = $title;
+                    $imgInfo = array_pop($page["imageinfo"]);
+                    if ( $imgInfo["mediatype"] === 'BITMAP' &&
+                         $imgInfo["width"] > 200 &&
+                         $imgInfo["height"] > 280 ){
+
+                        $metaData = $imgInfo["extmetadata"];
+
+                        $attributionRequired = ('true' === @$metaData["AttributionRequired"]["value"]);
+                        $cred = '';
+                        if ($attributionRequired){
+                            $cred .= @$metaData["LicenseShortName"]["value"] ?: @$metaData["LicenseShortName"]["value"];
+                            $cred .= ', ';
+                            $cred .= implode(' ', array( strip_tags(@$metaData["Credit"]["value"]), strip_tags(@$metaData["Artist"]["value"]) ));
+                        }
+                        $images[] = array (
+                            "caption"   => strip_tags(@$metaData['ImageDescription']['value'] ?: ''),
+                            "credit"    => $cred,
+                            "url"       => $imgInfo['thumburl'],
+                            "sourceurl" => $imgInfo['descriptionurl'],
+
+                        );
+                    }
+                }
+
+            }
+        } else {
+            /* invalid page or no images */
+        }
+        global $gExternalLaureateDataCacheTime;
+        __c()->set($md5, $images, $gExternalLaureateDataCacheTime*3600);
     }
-    global $gExternalLaureateDataCacheTime;
-    //__c()->set($md5, $images, $gExternalLaureateDataCacheTime*3600);
+    $output = array_merge( $images, $output );
+
 }
 
-echo json_encode( array( $laureate => $images ) );
+
+
+echo json_encode( array( $laureate => $output ) );
