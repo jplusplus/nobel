1fb282932916d15bc4ff925c078cff10106b0d93
rotsee <leo_wallentin@hotmail.com>
2015-12-01 15:40:18 +0100

    use standard ini notation

--- settings.example.php
+++ settings.example.php
@@ -101,6 +101,6 @@ $debugLevel = DEBUG;
 /* ----------------------------------------------- */
 
 if ($debugLevel == DEBUG){
-    ini_set('display_errors', 'On');
+    ini_set('display_errors', '1');
     error_reporting(E_ALL | E_STRICT);
 }
