ef1aeb73c530120eeff77c46f9b75f0ab6ba9d13
rotsee <leo_wallentin@hotmail.com>
2015-12-04 13:07:04 +0100

    update url to reflect form selections

--- js/ui/init.js
+++ js/ui/init.js
@@ -12,11 +12,13 @@ function initUI() {
             var filter = {};
             filter[key] = value;
             $toplist.data("filterset").changeFilter(filter);
+            if (gToplistSettings.updateUrl && (typeof (history.pushState) != "undefined") ) {
+                var urlPath = window.location.pathname + "?" + $form.serialize();
+                window.history.pushState("toplist-ui", "", urlPath);
+            }
         })
     });
 }
 $(document).ready(function() {
     initUI();
 });
-
-
