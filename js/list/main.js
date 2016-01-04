a85b6999da0808bb44f1450af764bc71eb69b6fb
rotsee <leo_wallentin@hotmail.com>
2016-01-04 11:42:08 +0100

    undo

--- js/list/main.js
+++ js/list/main.js
@@ -5,11 +5,18 @@
 ==============
 */ 
 
+
+var topLists = {};
 $(document).ready(function() {
 
     $(".toplist").each(function() {
         var $el = $(this);
+        var id = $el.attr("id");
+
         var filterset = new FilterSet(["gender", "award", "region", "popularity"], $el, gToplistSettings.endpoint);
+        topLists[id] = new TopList($el, filterset).initSparkLines();
     });
 
 });
+
+
