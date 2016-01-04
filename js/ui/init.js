3642e476cc3aebe3970b0dd5789626258416071f
rotsee <leo_wallentin@hotmail.com>
2016-01-04 10:50:33 +0100

    remove list related code from ui

--- js/ui/init.js
+++ js/ui/init.js
@@ -3,15 +3,7 @@ function initUI() {
     // Init filters
     $(".toplist-filter-ui").each(function() {
         var $form = $(this);
-        var $toplist = $( $form.attr("data-filter-for") );
         $form.find(".filter").on("change", function() {
-            var $select = $(this);
-            var key = $select.attr("name");
-            var value = $select.val();
-            value = (value == "null" && value == "") ? null : value;
-            var filter = {};
-            filter[key] = value;
-            $toplist.data("filterset").changeFilter(filter);
             if (gToplistSettings.updateUrl && (typeof (history.pushState) != "undefined") ) {
                 var urlPath = window.location.pathname + "?" + $form.serialize();
                 window.history.pushState("toplist-ui", "", urlPath);
